3c0acdb8ba7dd1f7bca7a98dec963fc78eead846
jsfraz <josefraz@seznam.cz>
2023-10-12 14:24:21 +0200

    On/off button. Improvements.

--- main.py
+++ main.py
@@ -3,35 +3,85 @@ import pynmea2
 from gnss_data import GNSSData
 import socketio
 import time
+import RPi.GPIO as GPIO
+import subprocess
+from threading import Thread
 
-SOCKETIO_URL = 'http://192.168.1.107:8080?apiKey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTUzOTUwOTcsIm5iZiI6MTY5NTM5NTA5Nywic3ViIjo0fQ.NCCdXFoQiiUpf614y5G80Zy3YEMp_fnqbKet9vivXyQ'
+PIN_SHUTDOWN_BUTTON = 5     # on/off pin
+PIN_PIEZO = 11      # piezo pin
+SOCKETIO_URL = 'http://192.168.1.107:8080?apiKey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2OTcxMDc2NjYsIm5iZiI6MTY5NzEwNzY2Niwic3ViIjozNn0.IhiW0vabOeIQd9fyuKtI-rfxAn8Ect8FGtC00iYg4do'
 SOCKETIO_RECCONECT_DELAY = 1
 SOCKETIO_INITIAL_CONNECT_DELAY = 10
 SERIAL_PORT = '/dev/serial0'
 BAUD_RATE = 115200
 SERIAL_TIMEOUT = 1
 
-# Connect to Socket.IO server
+# pin setup
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(PIN_SHUTDOWN_BUTTON, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(PIN_PIEZO, GPIO.OUT)
+
+# beep using piezo
+def beep(count, duration):
+    try:
+        pin = GPIO.PWM(PIN_PIEZO, 520)      # pwm instance
+        for x in range(count):
+            pin.start(50)       # start with duty cycle 50
+            pin.ChangeFrequency(520)
+            GPIO.output(PIN_PIEZO, GPIO.HIGH)
+            time.sleep(duration)
+            pin.stop()
+            GPIO.output(PIN_PIEZO, GPIO.LOW)
+            if x != count - 1:
+                time.sleep(0.3)
+    except Exception as e:
+        print(e)
+
+# Socket.IO server
 sio = socketio.Client(reconnection=True, reconnection_delay=SOCKETIO_RECCONECT_DELAY)
 
 # Define event handlers
 @sio.event
 def connect():
     print('Connected to the server.')
+    beep(2, 0.25)
 
 @sio.event
 def connect_error(data):
-    print('The connection failed: ' + data)
+    print(data)
+    beep(2, 1)
 
 @sio.event
 def disconnect():
     print('Disconnected from the server.')
 
+# listener for shutdown button
+def shutdown_button_listener():
+    pressed = False
+    while True:
+        # button is pressed when pin is LOW
+        if not GPIO.input(PIN_SHUTDOWN_BUTTON):
+            if not pressed:
+                print('Shutting down...')
+                sio.disconnect()
+                beep(1, 1)
+                # must be run as superuser!!!
+                subprocess.call(['shutdown', '-h', 'now'], shell=False)
+        # button not pressed (or released)
+        else:
+            pressed = False
+        time.sleep(0.1)
+
+# start listening for shutdown button press in separated thread
+shutdown_button_thread = Thread(target=shutdown_button_listener, name='shutdownButton')
+shutdown_button_thread.start()
+
+beep(1, 0.25)
+time.sleep(1)
 canConnect = False
 while canConnect == False:
 
     try:
-        # TODO pass the API key as parameter
         sio.connect(SOCKETIO_URL)
         canConnect = True
     except Exception as e:
